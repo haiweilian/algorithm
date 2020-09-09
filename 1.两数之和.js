6d4123aa0a4992028a382dcef4b8e270b3db4391
haiweilian <haiweilian@foxmail.com>
2020-09-09 23:17:53 +0800

    两数之合

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,8 +10,38 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
 
-};
+// @双层循环方案 ====================================================
+// 双层循环的时间复杂度 O(n^2)
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = 0; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target && i !== j) {
+//         return [i, j]
+//       }
+//     }
+//   }
+// }
+
+// @标识方案 ====================================================
+// 用对象存储目标值时间复杂度O(n)
+var twoSum = function (nums, target) {
+  let obj = {}
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (num in obj) {
+      // 如果当前值在目标，就找到了结果，取出索引
+      return [obj[num], i]
+    } else {
+      // 如果没有找到，存储下需要的目标
+      // 如果此次循环是 2, 那么我的目标值就是 7 索引是 1
+      obj[target - num] = i
+    }
+  }
+}
 // @lc code=end
 
+const nums = [2, 7, 11, 15]
+const target = 9
+
+console.log(twoSum(nums, target))
